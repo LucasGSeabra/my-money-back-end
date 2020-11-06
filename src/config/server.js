4062f70e7684151b52f46d1afc9bf376c872e1c4
Lucas Seabra <lucasgseabra@gmail.com>
2020-11-06 13:30:59 -0300

    Criado Server

---
+++ src/config/server.js
@@ -0,0 +1,14 @@
+import bodyParser from 'body-parser'
+import express from 'express'
+
+const server = express()
+const port = 3003
+
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+
+server.listen(port, function() {
+    console.log(`BACKEND is running on ${port}`)
+})
+
+
