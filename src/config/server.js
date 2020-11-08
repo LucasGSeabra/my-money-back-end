ee187b315fdeff746354b57f41ca2431df8926cd
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2020-11-08 12:43:17 -0300

    Adicionado queryParser em server

--- src/config/server.js
+++ src/config/server.js
@@ -1,6 +1,7 @@
 import bodyParser from 'body-parser'
 import express from 'express'
 import cors from '../config/cors.js'
+import queryParser from 'express-query-int'
 
 const server = express()
 const port = 3003
@@ -8,6 +9,7 @@ const port = 3003
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.use(cors)
+server.use(queryParser())
 
 server.listen(port, function() {
     console.log(`BACKEND is running on ${port}`)
