e43e736f889f1b4d36bb785a581088aa841017c6
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2020-11-08 12:30:12 -0300

    Adicionado cors em server

--- src/config/server.js
+++ src/config/server.js
@@ -1,11 +1,13 @@
 import bodyParser from 'body-parser'
 import express from 'express'
+import cors from '../config/cors.js'
 
 const server = express()
 const port = 3003
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(cors)
 
 server.listen(port, function() {
     console.log(`BACKEND is running on ${port}`)
