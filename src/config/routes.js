ceb8acc798275d098b76380c6ba70a354a457fac
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2021-01-21 17:03:26 -0300

    Adicionado rotas abertas

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import auth from './auth.js'
 import billingCycleService from '../api/billingCycle/billingCycleService.js'
-import billingCycle from '../api/billingCycle/billingCycleService.js'
+import { login, signUp, validateToken} from '../api/user/userService.js'
 
 function routes(server) {
     const protectedApi = express.Router()
@@ -9,6 +9,11 @@ function routes(server) {
     protectedApi.use(auth)
     billingCycleService.register(protectedApi, '/billingCycles')
 
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+    openApi.post('/login', login)
+    openApi.post('/signUp', signUp)
+    openApi.post('/validateToken', validateToken)
 }
 
 export default routes
